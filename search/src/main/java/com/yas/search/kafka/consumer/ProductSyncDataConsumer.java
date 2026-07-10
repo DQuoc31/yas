b0f47bcbb856e05cc21d582e926f8e1e64f93b6d
Quynhnhu0203 <163087967+Quynhnhu0203@users.noreply.github.com>
2026-07-11 01:50:20 +0700

    test 4

--- search/src/main/java/com/yas/search/kafka/consumer/ProductSyncDataConsumer.java
+++ search/src/main/java/com/yas/search/kafka/consumer/ProductSyncDataConsumer.java
@@ -47,7 +47,7 @@ public class ProductSyncDataConsumer extends BaseCdcConsumer<ProductMsgKey, Prod
     }
 
     // hihi test nè
-    // test típ típ típ típ
+    // test típ típ típ típ 22222
     public void sync(ProductMsgKey key, ProductCdcMessage productCdcMessage) {
         boolean isHardDeleteEvent = productCdcMessage == null || DELETE.equals(productCdcMessage.getOp());
         if (isHardDeleteEvent) {
